d923280c6c90c0d1e7e0811814f3b87111e5a66a
Dong Zhang <dzhang@quartermaster.house>
2021-02-03 23:34:12 -0500

    update colors component

--- src/clothes/Colors.js
+++ src/clothes/Colors.js
@@ -2,25 +2,23 @@ import React from 'react';
 
 const makeColor = (name, color, maskId) => {
   const Color = () => (
-    <g id='Color' mask={`url(${maskId}`} fillRule='evenodd' fill={color}>
-      <rect id='Color' x='0' y='0' width='264' height='110' />
+    <g
+      id={`color-${name}`}
+      mask={`url(#${maskId}`}
+      fillRule='evenodd'
+      fill={color}
+    >
+      <rect id={`color-rect-${name}`} x='0' y='0' width='264' height='110' />
     </g>
   );
-  const component = Color;
-  component.displayName = name;
-  component.optionValue = name;
-  return component;
+  // const component = Color;
+  // component.displayName = name;
+  // component.optionValue = name;
+  return Color();
 };
 
-const Blue1 = ({ maskId }) => {
-  return (
-    <g id='Color' mask={`url(#${maskId}`} fillRule='evenodd' fill='#65C9FF'>
-      <rect id='Color' x='0' y='0' width='264' height='110' />
-    </g>
-  );
-
-  // return makeColor('Blue1', '#65C9FF', maskId)
-};
+const Blue1 = ({ maskId }) => makeColor('blue1', '#65C9FF', maskId);
+const Blue2 = ({ maskId }) => makeColor('blue2', '#5199E4', maskId);
 
 const Colors = ({ maskId }) => <Blue1 maskId={maskId} />;
 
