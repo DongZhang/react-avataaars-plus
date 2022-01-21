49ce6fa3bf514d13b7434afd6408e63430703aa4
Dong Zhang <zhangdongopq@gmail.com>
2022-01-20 23:37:44 -0500

    add milk, tea and ice cream

---
+++ src/handheld/Milk.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Milk = () => {
+  return (
+    <g
+      id="Milk"
+      stroke="none"
+      strokeWidth="1"
+      fill="none"
+      fillRule="evenodd"
+      transform="translate(140, 158)"
+    >
+      <g id="milk">
+        <rect
+          id="Rectangle"
+          fill="#99ABD5"
+          fillRule="nonzero"
+          x="47"
+          y="0"
+          width="39"
+          height="10"
+        ></rect>
+        <polygon
+          id="Path"
+          fill="#B8C9F1"
+          fillRule="nonzero"
+          points="38 122 94 122 94 18.6666667 84.6666667 17 47.3333333 17 38 18.6666667"
+        ></polygon>
+        <path
+          d="M55.9621824,105 L77.0378176,105 C81.9871972,105 86,100.987788 86,96.0378176 L86,74.9621824 C86,70.0128028 81.9877881,66 77.0378176,66 L55.9621824,66 C51.0128028,66 47,70.0122119 47,74.9621824 L47,96.0378176 C47,100.987788 51.0122119,105 55.9621824,105 Z"
+          id="Path"
+          fill="#3498DB"
+          fillRule="nonzero"
+        ></path>
+        <polygon
+          id="Path"
+          fill="#ECF0F1"
+          fillRule="nonzero"
+          points="38 18 94 18 85.8165786 8 46.2352941 8"
+        ></polygon>
+        <rect id="Rectangle" x="0" y="0" width="132" height="122"></rect>
+      </g>
+    </g>
+  );
+};
+
+export default Milk;
--- src/handheld/index.js
+++ src/handheld/index.js
@@ -6,12 +6,18 @@ import Beer from './Beer';
 import Fries from './Fries';
 import IceWater from './IceWater';
 import Juice from './Juice';
+import Milk from './Milk';
+import IceCream from './IceCream';
+import Tea from './Tea';
 
 const componentsMap = {
   beer: Beer,
   fries: Fries,
   iceWater: IceWater,
   juice: Juice,
+  milk: Milk,
+  iceCream: IceCream,
+  tea: Tea,
 };
 
 const Handheld = () => (
