a9a91c2dfb8a1cc5ad5fa7603f8ea714a5d79f16
Dong Zhang <zhangdongopq@gmail.com>
2022-01-29 21:49:06 -0500

    update tea svg to solve conflict with circled background

--- example/src/App.js
+++ example/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
-import { Avatar, Piece } from 'react-avatar-plus';
+import { Avatar } from 'react-avatar-plus';
 
 const App = () => {
   return (
-    <>
-      <Avatar
-        avatarStyle="circle"
-        style={{ height: 280, width: 280 }}
-        avatarConfig={{
-          skinColor: 'WarmIvory',
-        }}
-      />
-    </>
+    <Avatar
+      avatarStyle="circle"
+      style={{ height: 280, width: 280 }}
+      avatarConfig={{
+        backgroundColor: 'blue03',
+        skinColor: 'warmIvory',
+        topType: 'shortHairDreads01',
+        hairColor: 'black',
+        eyeType: 'happy',
+        eyebrowType: 'default',
+        mouthType: 'smile',
+        clothesType: 'hoodie',
+        clothesColor: 'pastelBlue',
+        handheldType: 'tea'
+      }}
+    />
   );
 };
 
